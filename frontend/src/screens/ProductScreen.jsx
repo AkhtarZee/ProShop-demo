b58a877d1b31fcbd03707f06727368cf00b28188
Zeeshan <akhtar.zsn@gmail.com>
2023-11-07 17:06:49 +0530

    mod1 Complete

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -37,7 +37,10 @@ const ProductScreen = () => {
                 text={`${product.numReviews} reviews`}
               />
             </ListGroup.Item>
-            <ListGroup.Item>Price: $ {product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Price: $ <b>{product.price}</b>
+            </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
@@ -60,6 +63,15 @@ const ProductScreen = () => {
                     </strong>
                   </Col>
                 </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
               </ListGroupItem>
             </ListGroup>
           </Card>

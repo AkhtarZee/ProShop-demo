f14e591c6ea481d868ca1f9187c317c904cebf29
Zeeshan <akhtar.zsn@gmail.com>
2023-11-14 18:35:53 +0530

    Backend port andenv issue resolved.

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+dotenv.config();
+
+const port = process.env.PORT || "3400";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/products", (req, res) => {
+  res.send(products);
+  console.log(port);
+});
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.send(product);
+});
+
+app.listen(port, () => {
+  console.log(`Running on Port ${port}`);
+});
